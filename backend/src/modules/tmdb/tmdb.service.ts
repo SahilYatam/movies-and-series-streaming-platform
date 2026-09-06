573eb7b364b1037923705134d1dbb6ace980c205
Sahil Yatam <sahilyatam9@gmail.com>
2026-09-07 00:26:27 +0530

    backend(refactor): improve tmdb types

--- backend/src/modules/tmdb/tmdb.service.ts
+++ backend/src/modules/tmdb/tmdb.service.ts
@@ -1,60 +1,82 @@
 import { tmdbClient } from "./tmdb.client.js";
-import { TmdbPaginatedResponse, TmdbTitle } from "./tmdb.types.js";
+import {
+    TmdbEpisodeDetails,
+    TmdbPaginatedResponse,
+    TmdbSeasonDetails,
+    TmdbTitle,
+    TmdbTvDetails,
+} from "./tmdb.types.js";
 
 const getTrendingTitles = async () => {
-    const res = await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>("/trending/all/week");
+    const res =
+        await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>(
+            "/trending/all/week",
+        );
 
     return res.data.results.slice(0, 10);
 };
 
 const getPopularMovies = async () => {
-    const res = await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>("/movie/popular");
+    const res =
+        await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>(
+            "/movie/popular",
+        );
 
     return res.data.results;
 };
 
 const getPopularTv = async () => {
-    const res = await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>("/tv/popular");
+    const res =
+        await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>("/tv/popular");
 
     return res.data.results;
 };
 
 const getTopRatedMovies = async () => {
-    const res = await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>("/movie/top_rated");
+    const res =
+        await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>(
+            "/movie/top_rated",
+        );
 
     return res.data.results;
 };
 
 const getTopRatedTv = async () => {
-    const res = await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>("/tv/top_rated");
+    const res =
+        await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>("/tv/top_rated");
 
     return res.data.results;
 };
 
 const searchTitles = async (title: string) => {
-    const res = await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>("/search/multi", {
-        params: {
-            title,
+    const res = await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>(
+        "/search/multi",
+        {
+            params: {
+                title,
+            },
         },
-    });
+    );
 
     return res.data.results;
 };
 
 const getMovieDetails = async (movie_id: string) => {
-    const res = await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>(`/movie/${movie_id}`);
+    const res = await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>(
+        `/movie/${movie_id}`,
+    );
 
     return res.data;
 };
 
 const getTvSeriesDetails = async (series_id: string) => {
-    const res = await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>(`/tv/${series_id}`);
+    const res = await tmdbClient.get<TmdbTvDetails>(`/tv/${series_id}`);
 
     return res.data;
 };
 
 const getTvSeasonDetails = async (series_id: string, season_number: string) => {
-    const res = await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>(
+    const res = await tmdbClient.get<TmdbSeasonDetails>(
         `/tv/${series_id}/season/${season_number}`,
     );
 
@@ -66,7 +88,7 @@ const getTvEpisodeDetails = async (
     season_number: string,
     episode_number: string,
 ) => {
-    const res = await tmdbClient.get<TmdbPaginatedResponse<TmdbTitle>>(
+    const res = await tmdbClient.get<TmdbEpisodeDetails>(
         `/tv/${series_id}/season/${season_number}/episode/${episode_number}`,
     );
 
--- backend/src/modules/tmdb/tmdb.types.ts
+++ backend/src/modules/tmdb/tmdb.types.ts
@@ -1,4 +1,4 @@
-export interface TmdbPaginatedResponse<T>{
+export interface TmdbPaginatedResponse<T> {
     page: number;
     results: T[];
     total_pages: number;
@@ -8,14 +8,92 @@ export interface TmdbPaginatedResponse<T>{
 export interface TmdbTitle {
     id: number;
     media_type: "movie" | "tv";
+
     title?: string;
     name?: string;
+
     original_title?: string;
     original_name?: string;
+
     overview: string | null;
+
     poster_path: string | null;
     backdrop_path: string | null;
+
     original_language: string;
+
     vote_average: number;
+
     adult: boolean;
+}
+
+export interface TmdbTvSeasonSummary {
+    id: number;
+
+    name: string;
+    overview: string | null;
+
+    poster_path: string | null;
+
+    air_date: string | null;
+
+    season_number: number;
+    episode_count: number;
+}
+
+export interface TmdbTvDetails {
+    id: number;
+
+    name: string;
+    original_name: string;
+
+    overview: string | null;
+
+    poster_path: string | null;
+    backdrop_path: string | null;
+
+    original_language: string;
+
+    first_air_date: string | null;
+
+    vote_average: number;
+
+    adult: boolean;
+
+    seasons: TmdbTvSeasonSummary[];
+}
+
+export interface TmdbSeasonDetails {
+    id: number;
+
+    name: string;
+    overview: string | null;
+
+    poster_path: string | null;
+
+    air_date: string | null;
+
+    season_number: number;
+
+    episodes: TmdbEpisode[];
+}
+
+export interface TmdbEpisode {
+    id: number;
+
+    name: string;
+    overview: string | null;
+
+    episode_number: number;
+    season_number: number;
+
+    still_path: string | null;
+
+    air_date: string | null;
+
+    vote_average: number;
+}
+
+export interface TmdbEpisodeDetails extends TmdbEpisode {
+    production_code: string | null;
 }
